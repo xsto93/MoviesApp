9eaae25260694dafd26bb7ac2007868bd9460f2d
xsto93 <xsto93@gmail.com>
2022-11-14 21:42:36 +0100

    feat: adding cards in search with hardcoded criteria

--- src/views/movies-search/movies-search.tsx
+++ src/views/movies-search/movies-search.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { useMovies } from '../../shared/hooks/useMovies'
+import MovieCard from './movie-card/movie-card'
+
+import './movies-search.css'
 
 const MoviesSearch = (): JSX.Element => {
   const { movies } = useMovies('a')
 
   return (
-    <section>
-      <pre>{JSON.stringify(movies)}</pre>
+    <section className='movies__list '>
+      {movies?.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          image={movie.poster_path}
+          title={movie.title}
+          date={movie.release_date}
+        />
+      ))}
     </section>
   )
 }
